b20a9a6f7d37424c0804e9cc6872648b43bd5651
schmittrigger <schmittrigger@aarne.de>
2013-07-03 11:51:47 +0200

    Bidirectional shipping detection

--- modules/pi_ratepay/pi_ratepay_oxorder.php
+++ modules/pi_ratepay/pi_ratepay_oxorder.php
@@ -40,6 +40,19 @@ class pi_ratepay_oxorder extends pi_ratepay_oxorder_parent
      */
     private $_ratepayPaymentMethodUpperCase = '';
 
+    function save() {
+        $rp_paymenttype = (in_array($this->oxorder__oxpaymenttype->rawValue, pi_ratepay_util_utilities::$_RATEPAY_PAYMENT_METHOD)) ? true :false;
+
+        $deliverydate = is_null($this->oxorder__oxsenddate->rawValue) ? false : true;
+
+        if($deliverydate && $rp_paymenttype) {
+            $delivery = new pi_ratepay_Details();
+            $delivery->deliver();
+        }
+
+        parent::save();
+    }
+
     /**
      * {@inheritdoc}
      *
